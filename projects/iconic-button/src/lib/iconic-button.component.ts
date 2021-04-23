46d54743c3131f0358feb0d0d75127244a0e06c7
Philipp Schweiger <phips0812@gmail.com>
2021-04-23 16:21:19 +0200

    feat: adapted style of iconic button

--- projects/iconic-button/src/lib/iconic-button.component.ts
+++ projects/iconic-button/src/lib/iconic-button.component.ts
@@ -3,9 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'lib-iconic-button',
   template: `
-    <button (click)="onClick()">phips0812</button>
+    <button class="iconic-button" (click)="onClick()">phips0812</button>
   `,
   styles: [
+    '.iconic-button {' +
+    '  background-color: #4CAF50; /* Green */' +
+    '  border: none;' +
+    '  color: white;' +
+    '  padding: 15px 32px;' +
+    '  text-align: center;' +
+    '  text-decoration: none;' +
+    '  display: inline-block;' +
+    '  font-size: 16px;' +
+    '}'
   ]
 })
 export class IconicButtonComponent implements OnInit {
